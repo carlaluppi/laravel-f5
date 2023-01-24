d64baf902327c2f9d824520b8efbbe41ba2767a6
carlilu <carluppi99@gmail.com>
2023-01-24 12:23:48 +0100

    installed bootstrap

--- database/migrations/2023_01_24_094223_create_events_table.php
+++ database/migrations/2023_01_24_094223_create_events_table.php
@@ -13,15 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
-            $table->string('roll');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('events');
-            $table->rememberToken();
+            $table->string('title');
+            $table->dateTime('date');
+            $table->integer('maxUsers');
+            $table->longText('description');
+            $table->string('image');
+            $table->string('link');
             $table->timestamps();
         });
     }
@@ -33,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('events');
     }
 };
