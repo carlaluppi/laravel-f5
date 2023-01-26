8dccb809fadf5e3a8b38651f09e4925d8f952cec
carlilu <carluppi99@gmail.com>
2023-01-26 10:41:02 +0100

    installed ui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,23 @@
-<x-layouts.app title="Home">
+@extends('layouts.app')
 
-<h1 class="display-1">Home</h1>
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
 
-</x-layouts.app>
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
