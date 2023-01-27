e132b91a9ba2b89435340aee3830e4c83537305a
Rodrigo <rodrigo@asmalljob.com>
2023-01-27 11:30:38 +0100

    eventos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,10 @@
     <section class="container">
         <h1 class="display-1">Home</h1>
     </section>
+    <section>
+        <a href="{{ route('events.index') }}"> Events</a>
+       </section>
+
 </x-layouts.app>
 
 
